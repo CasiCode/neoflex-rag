0d572ac63383ccf7137b06e8d157385e3811c88d
casi <pitsunovtimur@gmail.com>
2025-06-02 00:58:30 +0400

    change api system

--- constants.py
+++ constants.py
@@ -3,8 +3,6 @@ CONTACTS_URL = 'https://www.neoflex.ru/contacts'
 CAREER_URL = 'https://www.neoflex.ru/about/career'
 CUSTOMERS_URL = 'https://www.neoflex.ru/about/customers'
 
-APIFY_GET_API = 'https://api.apify.com/v2/datasets/oDw3TPSSsJ2dZ4ayz/items?clean=true&format=json'
-
 UVICORN_SERVER_HOST = '127.0.0.1'
 
 UVICORN_SERVER_PORT = 8000
